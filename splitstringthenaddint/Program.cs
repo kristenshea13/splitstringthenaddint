7ede0a0e3e70dbf2aa8c974391593e7aaf32333a
Kristen Shea <kstar61@live.com>
2019-03-16 15:54:21 -0500

    /

--- splitstringthenaddint/Program.cs
+++ splitstringthenaddint/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace splitstringthenaddint
 {
@@ -6,7 +8,30 @@ namespace splitstringthenaddint
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string numbers = "1, 2, 3";
+
+            string[] numbersSplit = numbers.Split(", ");
+
+            var numbersInt = new List<int>();
+
+            foreach (var item in numbersSplit)
+            {
+                int number = int.Parse(item);
+                numbersInt.Add(number);
+                
+            }
+            int total = 0;
+
+            foreach (var number in numbersInt)
+            {
+                total = numbersInt.Sum();
+            }
+
+            Console.WriteLine($"The total of all numbers added from this list is: {total}");
+            Console.ReadLine();
+
+
+
         }
     }
 }
